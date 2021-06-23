7c729dcbcea07b66bb24fdb3e8ed3d266695be0d
Bryan Flores <bryancrack016@gmail.com>
2021-06-23 18:39:25 -0500

    Deber Clases

--- Modulo_1/basic/10_Clases.js
+++ Modulo_1/basic/10_Clases.js
@@ -1,3 +1,4 @@
+/*
 //Como se hacia antes
 
 function Rectangulo(base, altura) {
@@ -11,7 +12,7 @@ Rectangulo.prototype.calcularArea = function () {
 
 var rectangulo = new Rectangulo(6, 3);
 
-console.log(rectangulo.calcularArea());
+//console.log(rectangulo.calcularArea());
 
 // Declaracion de clases
 
@@ -26,9 +27,9 @@ class Rectangulo2 {
     }
 }
 
-var rectangulo2 = new Rectangulo2(3, 6);
+var rectangulo2 = new Rectangulo2(3, 6); */
 
-console.log(rectangulo2.calcularArea());
+//console.log(rectangulo2.calcularArea());
 
 // Clase Persona
 // implementar getters, setters y realizar una implementacion
@@ -38,3 +39,70 @@ console.log(rectangulo2.calcularArea());
 /* 
     DEBER 
 */
+
+class Persona{
+
+    constructor(nombre, apellido, ci, edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ci = ci;
+        this.edad = edad;
+    }
+
+    get saludo(){
+        return `Hola mi nombre es ${this.nombre} ${this.apellido}, mi edad es ${this.edad} y tengo mi numero de cedula ${this.ci}`;
+    }
+
+    set agregar_persona(datos){
+
+        const vector = datos.split(" ");
+        this.nombre = vector[0];
+        this.apellido = vector[1];
+        this.ci = vector[2];
+        this.edad = vector[3]
+    }
+
+    traerPersona(){
+        return `Hola mi nombre es ${this.nombre} ${this.apellido}, mi edad es ${this.edad} y tengo mi numero de cedula ${this.ci}`;
+    }
+
+}
+
+var person = new Persona("Bryan", "Flores", "1724440092", 21);
+console.log(person.saludo);
+person.agregar_persona = "Andres Obando 171568155 22";
+console.log(person.saludo);
+
+
+class Punto {
+
+    constructor ( x , y ){
+      this.x = x;
+      this.y = y;
+    }
+  
+    static distancia ( a , b) {
+
+      const dx = a.x - b.x;
+      const dy = a.y - b.y;
+  
+      return Math.sqrt ( dx * dx + dy * dy );
+    }
+
+  }
+
+  var punto1 = new Punto(5, 4);
+  var punto2 = new Punto(9, 7);
+  
+  console.log ("La distancia entre los puntos es: "+Punto.distancia(punto1, punto2));
+
+  class estudiante extends Persona{
+
+    traerPersona(prom){
+        return `${this.nombre} tiene un promedio de ${prom}`;
+    }
+
+  }
+
+  var persona_estudiante = new estudiante("Bryan");
+  console.log(persona_estudiante.traerPersona(9.8));
