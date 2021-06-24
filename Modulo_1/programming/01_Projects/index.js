c2b46b9f17f6ec713de351451c3c6285e4a16a4a
Bryan Flores <bryancrack016@gmail.com>
2021-06-24 12:08:01 -0500

    Avance en servidor y Router

--- Modulo_1/programming/01_Projects/index.js
+++ Modulo_1/programming/01_Projects/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 
 //genero una aplicacion
@@ -7,6 +8,28 @@ const app = express();
 app.get('/', (req,res) => {
     res.send("Hola mundo con ExpressJs");
 });
+
+app.get("/saludo",(req, res) => {
+
+    //console.log(req.query);
+    const { query: {nombre, apellido} } = req;
+
+    //const nombre = req.query.nombre;
+    //const apellido = req.query.apellido;
+
+    res.json({
+        saludo: `Hola soy ${nombre} ${apellido}`
+    });
+
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+
+    const { params: {nombre} } = req;
+    res.json({
+        nombre: nombre
+    })
+})
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en http://localhost:3000");
---
+++ Modulo_1/programming/01_Projects/routes/index.js
@@ -0,0 +1,11 @@
+// Traemos el paquete
+const { Router } = require('express');
+
+//Inicializamos Router
+const router = Router();
+
+//Creacion de los endpoint
+
+
+//Exportamos las rutas
+module.exports.RouterIndex = router;
